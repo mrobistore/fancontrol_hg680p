a766e38bd3a234167d7a23c667f88ea517fbbbc8
mrobistore <mrobistore@gmail.com>
2024-11-17 08:01:00 +0700

    Update

---
+++ install_fan_control.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Clone repository
+echo "Mengunduh file dari GitHub..."
+git clone https://github.com/mrobistore@gmail.com/fan-control.git /tmp/fan_control
+
+# Salin file ke lokasi yang sesuai
+echo "Menyalin file konfigurasi..."
+cp -r /tmp/fan_control/etc /etc
+cp -r /tmp/fan_control/root /root
+cp -r /tmp/fan_control/usr /usr
+
+# Beri izin eksekusi pada file init dan script
+chmod +x /etc/init.d/fan_control
+chmod +x /root/fan_control.sh
+
+# Tambahkan layanan fan_control ke startup
+echo "Menambahkan layanan fan_control ke startup..."
+/etc/init.d/fan_control enable
+/etc/init.d/fan_control start
+
+# Hapus file sementara
+rm -rf /tmp/fan_control
+
+echo "Instalasi selesai."
